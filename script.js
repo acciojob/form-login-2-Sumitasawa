a2897d09fe7dac7811a1d4d415551ac08440c044
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-12 13:20:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-// your JS code here
-document.getElementById("btn").addEventListener("click", function (event) {
-    event.preventDefault(); 
 
-    let fname = document.getElementById("First Name").value.trim();
-    let lname = document.getElementById("Last Name").value.trim();
-    let pnumber = document.getElementById("Phone Number").value.trim();
-    let email = document.getElementById("Email ID").value.trim();
+function getFormvalue() {
+	let fname = document.getElementById("First Name").value.trim();
+	let lname = document.getElementById("Last Name").value.trim();
+	let pnumber = document.getElementById("Phone Number").value.trim();
+	let email = document.getElementById("Email ID").value.trim();
 
-    alert(
-        "First Name: " + fname + "\n" +
-        "Last Name: " + lname + "\n" +
-        "Phone Number: " + pnumber + "\n" +
-        "Email ID: " + email
-    );
-});
+	alert(
+		"First Name: " + fname + "\n" +
+		"Last Name: " + lname + "\n" +
+		"Phone Number: " + pnumber + "\n" +
+		"Email ID: " + email
+	);
+}
