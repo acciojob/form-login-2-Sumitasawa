beaae7d544ea0b82690477b73bae11346c2bd429
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-12 13:15:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+// your JS code here
+document.getElementById("btn").addEventListener("click", function (event) {
+    event.preventDefault(); // stops form from reloading page
+
+    let fname = document.getElementById("First Name").value.trim();
+    let lname = document.getElementById("Last Name").value.trim();
+    let pnumber = document.getElementById("Phone Number").value.trim();
+    let email = document.getElementById("Email ID").value.trim();
+
+    alert(
+        "First Name: " + fname + "\n" +
+        "Last Name: " + lname + "\n" +
+        "Phone Number: " + pnumber + "\n" +
+        "Email ID: " + email
+    );
+});
